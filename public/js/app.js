4dcc92fcadfd6db99f251d54527569f28d634ebc
furatmalkawi29 <malkawifurat29@gmail.com>
2021-04-15 02:38:56 +0300

    done css

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,20 @@
 
+
+//new.ej ----------------------
 $('#updateForm').hide();
 
 $('#updateBtn').on('click',function(){
+  $('.bookContain').hide();
   $('#updateForm').toggle();
+
+});
+
+
+
+//header -------------------
+
+$('nav').hide();
+
+$('header i').on('click',function(){
+  $('nav').toggle();
 });
